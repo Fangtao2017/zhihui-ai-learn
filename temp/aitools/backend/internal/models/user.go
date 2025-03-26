5f4bdcd9c0d835500c9b2ebf93a15ed80425fce1
Fangtao2017 <114807994+Fangtao2017@users.noreply.github.com>
2025-03-26 12:56:34 +0800

    Clean up repository structure

--- temp/aitools/backend/internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       string `json:"id,omitempty" bson:"_id,omitempty"`
-	Username string `json:"username" bson:"username"`
-	Email    string `json:"email" bson:"email"`
-	Password string `json:"password" bson:"password"`
-}
